4e4054f94f9e187d5b57fb66a69b6308ad3636fb
MarkoABC98 <alanpowermx@gmail.com>
2020-03-29 14:36:33 -0600

    added button features and purchase, popups

---
+++ js/popup.js
@@ -0,0 +1,123 @@
+													/* ------------------------- */
+													/* Equipos Escritorio ------ */
+													/* ------------------------- */
+
+/* ------------------------- */
+/* Equipos Escritorio 00 --- */
+/* ------------------------- */
+
+var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+	overlay = document.getElementById('overlay'),
+	popup = document.getElementById('popup'),
+	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+btnAbrirPopup.addEventListener('click', function(){
+	overlay.classList.add('active');
+	popup.classList.add('active');
+});
+
+btnCerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	popup.classList.remove('active');
+});
+/* ------------------------- */
+/* Equipos Escritorio 01 --- */
+/* ------------------------- */
+
+var btnAbrirPopup01 = document.getElementById('btn-abrir-popup01'),
+	overlay01 = document.getElementById('overlay01'),
+	popup01 = document.getElementById('popup01'),
+	btnCerrarPopup01 = document.getElementById('btn-cerrar-popup01');
+
+btnAbrirPopup01.addEventListener('click', function(){
+	overlay01.classList.add('active');
+	popup01.classList.add('active');
+});
+
+btnCerrarPopup01.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay01.classList.remove('active');
+	popup01.classList.remove('active');
+});
+
+
+/* ------------------------- */
+/* Equipos Escritorio 02 --- */
+/* ------------------------- */
+
+var btnAbrirPopup02 = document.getElementById('btn-abrir-popup02'),
+	overlay02 = document.getElementById('overlay02'),
+	popup02 = document.getElementById('popup02'),
+	btnCerrarPopup02 = document.getElementById('btn-cerrar-popup02');
+
+btnAbrirPopup02.addEventListener('click', function(){
+	overlay02.classList.add('active');
+	popup02.classList.add('active');
+});
+
+btnCerrarPopup02.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay02.classList.remove('active');
+	popup02.classList.remove('active');
+});
+
+/* ------------------------- */
+/* Equipos Laptops 03    --- */
+/* ------------------------- */
+
+var btnAbrirPopup03 = document.getElementById('btn-abrir-popup03'),
+	overlay03 = document.getElementById('overlay03'),
+	popup03 = document.getElementById('popup03'),
+	btnCerrarPopup03 = document.getElementById('btn-cerrar-popup03');
+
+btnAbrirPopup03.addEventListener('click', function(){
+	overlay03.classList.add('active');
+	popup03.classList.add('active');
+});
+
+btnCerrarPopup03.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay03.classList.remove('active');
+	popup03.classList.remove('active');
+});
+
+/* ------------------------- */
+/* Equipos Laptops 04    --- */
+/* ------------------------- */
+
+var btnAbrirPopup04 = document.getElementById('btn-abrir-popup04'),
+	overlay04 = document.getElementById('overlay04'),
+	popup04 = document.getElementById('popup04'),
+	btnCerrarPopup04 = document.getElementById('btn-cerrar-popup04');
+
+btnAbrirPopup04.addEventListener('click', function(){
+	overlay04.classList.add('active');
+	popup04.classList.add('active');
+});
+
+btnCerrarPopup04.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay04.classList.remove('active');
+	popup04.classList.remove('active');
+});
+
+/* ------------------------- */
+/* Equipos Laptops 05    --- */
+/* ------------------------- */
+
+var btnAbrirPopup05 = document.getElementById('btn-abrir-popup05'),
+	overlay05 = document.getElementById('overlay05'),
+	popup05 = document.getElementById('popup05'),
+	btnCerrarPopup05 = document.getElementById('btn-cerrar-popup05');
+
+btnAbrirPopup05.addEventListener('click', function(){
+	overlay05.classList.add('active');
+	popup05.classList.add('active');
+});
+
+btnCerrarPopup05.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay05.classList.remove('active');
+	popup05.classList.remove('active');
+});
